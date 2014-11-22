7f7bd3fe8db2c2529be49d526839f12ab98368bc
Dariusz Niemczyk <palid@o2.pl>
2014-11-22 07:49:51 +0100

    Even more parsing in  parser

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var shortID = require('short-id');
 
 // Config and inside libs
 var config = require('./config/game.js');
-var joinCMD = require('./sockets/join.js');
 
 shortID.configure({
   length: 4, // The length of the id strings to generate
@@ -35,7 +34,8 @@ function newGame(userId) {
     },
     beacons: [],
     players: [],
-    host: userId
+    host: userId,
+    state: "lobby"
   };
   return game;
 }
@@ -51,12 +51,26 @@ function commonResponse(status, obj) {
   return getSingleRequest(_.extend(response, obj));
 }
 
+function errorBuilder(command, description) {
+  return getSingleRequest({
+    status: "ERR",
+    description: description,
+    command: command,
+    date: Date.now()
+  });
+}
+
 function returnGameUsers(gameId) {
   var game = currentGames[gameId];
+  game.status = "OK";
+  game.date = Date.now();
   return _.transform(game, function(result, item, key) {
     if (key === 'players') {
       result.players = _.map(game.players, function(item) {
-        return item.socket.user.userId;
+        return {
+          userId: item.socket.user.userId,
+          ready: item.socket.user.ready
+        };
       });
     } else {
       result[key] = item;
@@ -70,7 +84,7 @@ function returnGameUsers(gameId) {
  * @return {[type]}        [description]
  */
 var server = net.createServer(config, function(socket) {
-  console.log("user connected");
+  console.log("Server noticed that user connected.");
   socket.setEncoding("utf8");
   socket.on('data', function(data) {
     console.log("received data:");
@@ -110,60 +124,88 @@ var server = net.createServer(config, function(socket) {
           socket.write(getSingleRequest(returnGameUsers(gameId)));
           break;
         case "JOIN":
+        if (parsed && parsed.request && parsed.request.gameId) {
           gameId = parsed.request.gameId;
           game = currentGames[gameId];
           if (game) {
-            if (game.settings.maxPlayers > game.players.length) {
-              game.players.push({
-                socket: socket
-              });
-              socket.game = game;
-              currentGames[gameId] = game;
-              game.players.forEach(function(item) {
-                item.socket.write(getSingleRequest(returnGameUsers(gameId)));
-              });
+            if (game.state === "lobby") {
+              if (game.settings.maxPlayers > game.players.length) {
+                game.players.push({
+                  socket: socket
+                });
+                socket.game = game;
+                currentGames[gameId] = game;
+                game.players.forEach(function(item) {
+                  item.socket.write(getSingleRequest(returnGameUsers(gameId)));
+                });
+              } else {
+                socket.write(errorBuilder("JOIN", "Maximum players amount exceeded."));
+              }
             } else {
-              socket.write(getSingleRequest({
-                status: "ERR",
-                description: "Maximum players amount exceeded.",
-                date: Date.now,
-                command: parsed && parsed.command ? parsed.command : 'undefined'
-              }));
+              socket.write(errorBuilder("JOIN", "Game already started or finished"));
             }
           } else {
-            socket.write(getSingleRequest({
-              status: "ERR",
-              description: "Game not found.",
-              date: Date.now,
-              command: parsed && parsed.command ? parsed.command : 'undefined'
-            }));
+            socket.write(errorBuilder("JOIN", "Game not found."));
           }
+        } else {
+          socket.write(errorBuilder("JOIN", "Wrong request!"));
+        }
           break;
         case "LOBBY_UPDATE":
+          if (parsed && parsed.request && parsed.request.gameId) {
+              gameId = parsed.request.gameId;
+              game = currentGames[gameId];
+              if (game.state === "lobby") {
+                var playerIndex = _.findIndex(game.players, {
+                  userId: parsed.request.userId
+                });
+                if (playerIndex !== -1) {
+                  if (parsed.request.userId === currentGames[gameId].host) {
+                    // Retarded 'anticheat'.
+                    var clearObj = _.transform(currentGames[gameId], function(result, item, key) {
+                      if (key !== "players" || key !== "host" || key !== "state") {
+                        result[key] = item;
+                      }
+                    });
+                    _.extend(clearObj, parsed.request);
+                  }
+                  if (!_.isUndefined(parsed.request.ready)) {
+                    game.players[playerIndex].socket.user.ready = parsed.request.ready;
+                  }
+                  game.players.forEach(function(item) {
+                    item.socket.write(getSingleRequest(returnGameUsers(gameId)));
+                  });
+                } else {
+                  socket.write(errorBuilder("LOBBY_UPDATE", "You're not in the game " + gameId));
+                }
+              } else {
+                socket.write(errorBuilder("LOBBY_UPDATE", "Game already started or finished."));
+              }
+          } else {
+            socket.write(errorBuilder("LOBBY_UPDATE", "Wrong request!"));
+          }
+          break;
+        case "GAME":
           if (parsed && parsed.request && parsed.request.gameId) {
             gameId = parsed.request.gameId;
             game = currentGames[gameId];
             if (parsed.request.userId === currentGames[gameId].host) {
-              // Retarded 'anticheat'.
-              var clearObj = _.transform(currentGames[gameId], function(result, item, key) {
-                if (key !== "players" || key !== "host") {
-                  result[key] = item;
-                }
-              });
-              _.extend(clearObj, parsed.request);
+              if (_.every(game.players, 'ready')) {
+              currentGames[gameId].state = 'started';
+                game.players.forEach(function(item) {
+                  item.socket.write(getSingleRequest(returnGameUsers(gameId)));
+                });
+              } else {
+                socket.write(errorBuilder("GAME",  _.countBy(game.players, function(player){
+                  return player.ready;
+                })  + " player(s) aren't ready yet."));
+              }
+            } else {
+              socket.write(errorBuilder("GAME", "You're not the host!"));
             }
-            if (!_.isUndefined(parsed.request.ready)) {
-              var playerIndex = _.findIndex(game.players, {
-                userId: parsed.request.userId
-              });
-              game.players[0].ready = parsed.request.ready;
-            }
-            game.players.forEach(function(item) {
-              item.socket.write(getSingleRequest(returnGameUsers(gameId)));
-            });
+          } else {
+            socket.write(errorBuilder("GAME", "Wrong request!"));
           }
-          break;
-        case "GAME":
           break;
         case "CAPTURE":
           break;
@@ -175,7 +217,6 @@ var server = net.createServer(config, function(socket) {
 });
 
 server.listen(8069);
-console.log(server.prototype);
 
 server.on('error', function(err) {
   if (err.code === "EADDRINUSE") {
@@ -204,12 +245,16 @@ client.write(getSingleRequest({
 
 client.write(getSingleRequest({
   "command": "HOST",
-  "request": {}
+  "request": {
+    userId: shortID.generate()
+  }
 }));
 
 
 client.on('data', function(data) {
+  console.log("Client 1");
   console.log(data);
+  console.log("End of client 1");
 });
 
 
@@ -238,7 +283,9 @@ client2.write(getSingleRequest({
 }));
 
 client2.on('data', function(data) {
+  console.log("CLIENT 2");
   console.log(data);
+  console.log("END OF CLIENT 2");
 });
 
 
@@ -249,3 +296,25 @@ client2.write(getSingleRequest({
     ready: false
   }
 }));
+
+setTimeout(function(){
+  client2.write(getSingleRequest({
+    command: "LOBBY_UPDATE",
+    request: {
+      gameId: '1234',
+      ready: false
+    }
+  }));
+
+}, 20);
+
+setTimeout(function(){
+  client2.write(getSingleRequest({
+    command: "LOBBY_UPDATE",
+    request: {
+      gameId: '1234',
+      ready: false
+    }
+  }));
+
+}, 30);
