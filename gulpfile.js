2c99f68ea200b8cb4326552cbc0718a577369b4e
Dariusz Niemczyk <palid@o2.pl>
2014-11-24 18:37:52 +0100

    Huge cleanup, tests addition and stuff.

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+"use strict";
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var flowtype = require('gulp-flowtype');
+var jest = require('gulp-jest');
+var nodemon = require('gulp-nodemon');
+
+var DIR = './lib/**/*.js';
+
+var JEST_OPTIONS = {
+  scriptPreprocessor: "./spec/support/preprocessor.js",
+  unmockedModulePathPatterns: [
+    "node_modules/react"
+  ],
+  testDirectoryName: "__tests__",
+  testPathIgnorePatterns: [
+    "node_modules",
+    "spec/support"
+  ],
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "react"
+  ]
+};
+
+gulp.task('lint', function() {
+  return gulp.src(DIR)
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('typecheck', function() {
+  return gulp.src(DIR)
+    .pipe(flowtype());
+});
+
+gulp.task('tests', function() {
+  return gulp.src('./lib/')
+    .pipe(jest());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(DIR, [
+    // 'typecheck',
+    'lint',
+    'tests'
+    ]);
+});
+// define tasks here
+gulp.task('default', [
+  // 'typecheck',
+  'lint',
+  'tests',
+  'watch'
+]);
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var loadDir = require('../util/load-directory.js');
-
-module.exports = loadDir(__dirname, {
-    currentDir: __dirname,
-    type: '.js',
-    recursive: true
-});
